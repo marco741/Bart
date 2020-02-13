1eae2a6a240bf1805655bbf6e5a1082cad14810f
marco741 <marco@ninjacampus.io>
2020-02-13 10:41:57 +0100

    Initial Commit

---
+++ Bart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bart
+//
+//  Created by Marco Della Rocca on 13/02/2020.
+//  Copyright © 2020 Marco Della Rocca. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
